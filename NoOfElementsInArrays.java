35fb50dc4209fb8dbfa5ec09f1912fed974e6d10
NehaBhosale10 <116482948+NehaBhosale10@users.noreply.github.com>
2022-10-31 23:06:40 +0530

    Create NoOfElementsInArrays.java

---
+++ NoOfElementsInArrays.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Example {
+ 
+  public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+    int m;
+    System.out.println("Enter no of elements");
+    m = sc.nextInt();
+    int numbers[] = new int[m];
+   
+    System.out.println("Number of elements in the given int array: " + numbers.length);
+
+   
+  }
+}
